b06c5cdb7587434553c0c52522821291f7c4ea93
Nonni <jonb18@ru.is>
2018-11-16 00:54:00 +0000

    f

---
+++ Lokapróf/try_except/try_open_except_value.py
@@ -0,0 +1,8 @@
+try:
+    f = open('myfile.txt')
+    s = f.readline()
+    i = int(s.strip())
+except OSError as err:
+    print("OS error: {0}".format(err))
+except ValueError:
+    print("Could not convert data to an integer.")
